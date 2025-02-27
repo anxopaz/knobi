6e3b36975fca5a33c837d8fb44ead1f7343d8b43
Anxo Paz Cuña <apaz>
2025-02-27 09:37:20 +0100

    residuals plot

--- R/error.R
+++ R/error.R
@@ -43,12 +43,6 @@ kbpm_error <- function( knobi_results, env_results=NULL, plot_out) {
       ggplot2::theme(legend.position = c(0.15,0.85), plot.title = ggplot2::element_text(hjust = 0.5),
                      plot.subtitle = ggplot2::element_text(hjust = 0.5),legend.title=ggplot2::element_blank(),
                      legend.background = ggplot2::element_rect(fill = "transparent"))
-    
-    if (plot_out==TRUE){
-      p <- grDevices::recordPlot()
-      grDevices::jpeg("residuals.jpeg",width=2500, height=2500,res=300)
-      grDevices::replayPlot(p)
-      grDevices::dev.off()}
 
   } else {
 
@@ -125,18 +119,19 @@ kbpm_error <- function( knobi_results, env_results=NULL, plot_out) {
                      plot.subtitle = ggplot2::element_text(hjust = 0.5),legend.title=ggplot2::element_blank(),
                      legend.background = ggplot2::element_rect(fill = "transparent"))
     
-    if (plot_out==TRUE){
-      p <- grDevices::recordPlot()
-      grDevices::jpeg("residuals_env.jpeg",width=2500, height=2500,res=300)
-      grDevices::replayPlot(p)
-      grDevices::dev.off()}
-
-  }
+    
 
   error<-list(error_table=error_table,residuals=residuals)
 
   print(res_plot)
-
+  
+  if (plot_out==TRUE){
+    p <- grDevices::recordPlot()
+    grDevices::jpeg(ifelse(is.null(env_results),"residuals.jpeg","residuals_env.jpeg"),width=2500, height=2500,res=300)
+    grDevices::replayPlot(p)
+    grDevices::dev.off()}
+  
+  }
 
   return(error)
 
